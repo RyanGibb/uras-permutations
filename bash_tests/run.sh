8b70961f92f9a95b22418e114d8f420855455fc5
RyanGibb <ryangibb321@gmail.com>
2020-06-12 21:46:59 +0100

    bash script test supports multiple executables

--- bash_tests/run.sh
+++ bash_tests/run.sh
@@ -1,12 +1,11 @@
 cd $(dirname "$0")/..
 
-executable=$1
-
-out_dir=$(dirname "$0")/$(basename "$executable")
-
-mkdir -p $out_dir
-
-for file in ./permutations/*; do
-	echo "$file"
-	cat "$file" | ./"$executable" | tail -n 1 > $out_dir/$(basename "$file" .txt).out
+for executable in "$@"; do
+	echo $executable
+	out_dir=$(dirname "$0")/$(basename "$executable")
+	mkdir -p $out_dir
+	for file in ./permutations/*; do
+		echo "$file"
+		cat "$file" | ./"$executable" | tail -n 1 > $out_dir/$(basename "$file" .txt).out
+	done
 done
