05f57abe88749f4f8c143c476a2fc28ec50a9838
RyanGibb <ryangibb321@gmail.com>
2020-06-16 17:15:45 +0100

    Fast benchmarking for quickly gauging performance

--- benchmarking/run_quick.sh
+++ benchmarking/run_quick.sh
@@ -3,7 +3,7 @@ out_file=$(dirname "$0")/benchmarks.csv
 REPETITIONS=3
 
 if [ ! -f $out_file ]; then
-    echo "Executable, n, Repetition, CPU Time (ns), Iterations" > $out_file
+    echo "Executable, n, CPU Time (ns), Iterations" > $out_file
 fi
 
 get_cpu_time () {
@@ -19,15 +19,12 @@ for executable in "$@"; do
 		echo "$file"
 		cpu_time_ns=0
 		new_iterations=1
-		while [ $cpu_time_ns -le 1000000000 ]; do # 1 second
+		while [ $cpu_time_ns -le 100000000 ]; do # 0.1 second
 			iterations=$new_iterations
 			cpu_time_ns=$(get_cpu_time)
 			echo "At $iterations iterations took $cpu_time_ns (ns)"
 			new_iterations=$(($iterations * 2))
 		done
-		echo $(basename "$executable" .sh),$(basename "$file" _pair.txt), 1, $cpu_time_ns, $iterations >> $out_file
-		for ((repetition=2; repetition<=$REPETITIONS; repetition++)); do
-			echo $(basename "$executable" .sh),$(basename "$file" _pair.txt), $repetition, $(get_cpu_time), $iterations >> $out_file
-		done
+		echo $(basename "$executable" .sh),$(basename "$file" _pair.txt), $cpu_time_ns, $iterations >> $out_file
 	done
 done
