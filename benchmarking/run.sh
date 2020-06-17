936fb61a2384cb71309ba3b867ab3dab8b208b24
Ryan Gibb <ryangibb321@gmail.com>
2020-06-17 23:43:47 +0100

    Updated benchmarking script with better logging

--- benchmarking/run.sh
+++ benchmarking/run.sh
@@ -5,7 +5,7 @@ TARGET_TIME_NS=1000000000 # 1 second
 PERMUTATION_FILES=$(ls -v /cs/scratch/rtg2/permutations/*.perm)
 
 if [ ! -f $out_file ]; then
-    echo "Executable, n, Repetition, CPU Time (ns), Iterations" > $out_file
+    echo "Date, Executable, n, Repetition, CPU Time (ns), Iterations" > $out_file
 fi
 
 get_cpu_time () {
@@ -18,18 +18,17 @@ cargo build --release --manifest-path $(dirname "$0")/../rust/Cargo.toml || exit
 for file in $PERMUTATION_FILES; do
 	echo "$file"
 	for executable in "$@"; do
-		echo $executable
+		echo $(date) $file $executable
 		cpu_time_ns=0
 		new_iterations=1
 		while [ $cpu_time_ns -le $TARGET_TIME_NS ]; do
 			iterations=$new_iterations
 			cpu_time_ns=$(get_cpu_time)
-			echo "At $iterations iterations took $cpu_time_ns (ns)"
 			new_iterations=$(($iterations * 2))
 		done
-		echo $(basename "$executable" .sh),$(basename "$file" .perm), 1, $cpu_time_ns, $iterations >> $out_file
+		echo $(date), $(basename "$executable" .sh), $(basename "$file" .perm), 1, $cpu_time_ns, $iterations >> $out_file
 		for ((repetition=2; repetition<=$REPETITIONS; repetition++)); do
-			echo $(basename "$executable" .sh),$(basename "$file" _pair.txt), $repetition, $(get_cpu_time), $iterations >> $out_file
+			echo $(date), $(basename "$executable" .sh), $(basename "$file" .perm), $repetition, $(get_cpu_time), $iterations >> $out_file
 		done
 	done
 done
