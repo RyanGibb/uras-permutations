3dfc2c82e11e91a78467e752211bb65d976f039a
RyanGibb <ryangibb321@gmail.com>
2020-07-13 14:06:38 +0100

    Changed n to size type.
    This allows permutations of degree exactly one more than the max value of the permutation type.

--- rust/src/composition_cooperman_ma.rs
+++ rust/src/composition_cooperman_ma.rs
@@ -9,7 +9,7 @@ pub static mut CACHE_SIZE: usize = 256;
 
 // Calculates z = xy, overwriting z. All of x, y, and z must of size n,
 // with x and y containing values from 0 to n - 1.
-pub fn composition_cooperman_ma(n: PermT, x: &[PermT], y: &[PermT], z: &mut [PermT]) {
+pub fn composition_cooperman_ma(n: usize, x: &[PermT], y: &[PermT], z: &mut [PermT]) {
 	// block_length, and therefor cache_size, should be a power of 2
     let mut block_length = unsafe { CACHE_SIZE } / 2 / size_of::<PermT>();
 	// Used for efficient right shift division by block_length
@@ -18,9 +18,9 @@ pub fn composition_cooperman_ma(n: PermT, x: &[PermT], y: &[PermT], z: &mut [Per
     block_length = 2usize.pow(block_length_shift as u32);
     
     // Integer division rounding up
-    let number_of_blocks = (n as usize + block_length - 1) >> block_length_shift;
+    let number_of_blocks = (n + block_length - 1) >> block_length_shift;
 
-    let mut d: Vec<PermT> = vec![0; n as usize];
+    let mut d: Vec<PermT> = vec![0; n];
     let mut d_ptr: Vec<*mut PermT> = Vec::with_capacity(number_of_blocks);
     unsafe {
         d_ptr.set_len(number_of_blocks);
@@ -31,15 +31,15 @@ pub fn composition_cooperman_ma(n: PermT, x: &[PermT], y: &[PermT], z: &mut [Per
     for block_num in 0..number_of_blocks {
         d_ptr[block_num] = &mut d[block_num * block_length];
     }
-    for i in 0..n as usize {
-        let block_num = (x[i] >> block_length_shift) as usize;
+    for i in 0..n {
+        let block_num = x[i] >> block_length_shift;
         unsafe { *d_ptr[block_num] = x[i] };
         d_ptr[block_num] = unsafe { d_ptr[block_num].offset(1) };
     }
 
     //Phase II: for d[i] == x[a], replace the value x[a] by y[x[a]]
     // Note that |i - d[i]| == |i - x[a]| and |i-x[a]| < block_length
-    for i in 0..n as usize {
+    for i in 0..n {
         d[i] = y[d[i] as usize];
     }
 
@@ -47,8 +47,8 @@ pub fn composition_cooperman_ma(n: PermT, x: &[PermT], y: &[PermT], z: &mut [Per
     for block_num in 0..number_of_blocks {
         d_ptr[block_num] = &mut d[block_num * block_length];
     }
-    for i in 0..n as usize {
-        let block_num = (x[i] >> block_length_shift) as usize;
+    for i in 0..n {
+        let block_num = x[i] >> block_length_shift;
         z[i] = unsafe { *d_ptr[block_num] };
         d_ptr[block_num] = unsafe { d_ptr[block_num].offset(1) };
     }
--- rust/src/generate_permutations.rs
+++ rust/src/generate_permutations.rs
@@ -9,29 +9,29 @@ mod composition;
 use composition::PermT;
 
 // Uses the Fisher–Yates shuffle to generate a random permutation of degree n
-fn generate_random_permutation(n: PermT, rng: &mut ThreadRng) -> Vec<PermT> {
-    let mut p: Vec<PermT> = vec![0; n as usize];
+fn generate_random_permutation(n: usize, rng: &mut ThreadRng) -> Vec<PermT> {
+    let mut p: Vec<PermT> = vec![0; n];
     for i in 0..n {
-        p[i as usize] = i;
+        p[i] = i;
     }
     for i in 0..n {
         let j = rng.gen_range(i, n);
-        let temp = p[j as usize];
-        p[j as usize] = p[i as usize];
-        p[i as usize] = temp;
+        let temp = p[j];
+        p[j] = p[i];
+        p[i] = temp;
     }
     p
 }
 
-fn generate_permutation_pair(n: PermT, rng: &mut ThreadRng) -> io::Result<()> {
+fn generate_permutation_pair(n: usize, rng: &mut ThreadRng) -> io::Result<()> {
     io::stdout().write_all(&n.to_le_bytes())?;
     let x = generate_random_permutation(n, rng);
     for i in 0..n {
-        io::stdout().write_all(&x[i as usize].to_le_bytes())?;
+        io::stdout().write_all(&x[i].to_le_bytes())?;
     }
     let y = generate_random_permutation(n, rng);
     for i in 0..n {
-        io::stdout().write_all(&y[i as usize].to_le_bytes())?;
+        io::stdout().write_all(&y[i].to_le_bytes())?;
     }
     Ok(())
 }
@@ -54,6 +54,14 @@ fn main() -> io::Result<()> {
             process::exit(1);
         }
     };
+    if n < 1 {
+        eprintln!("Please enter n greater or equal to 1");
+        process::exit(1);
+    }
+    if n - 1 > PermT::MAX {
+        eprintln!("Please enter n less than {}\n", PermT::MAX as u128 + 1);
+        process::exit(1);
+    }
     let mut rng = rand::thread_rng();
     generate_permutation_pair(n, &mut rng)?;
     Ok(())
