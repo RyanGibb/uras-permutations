1e568b038eec83e62ee302f1c6427cbc55cd1fc4
RyanGibb <ryangibb321@gmail.com>
2020-07-27 14:19:17 +0100

    Updated create_permutations to take the max permutation degree

--- util/create_permutations.sh
+++ util/create_permutations.sh
@@ -1,8 +1,9 @@
 cargo build --release --manifest-path $(dirname "$0")/../rust/Cargo.toml || exit 1
 
 dir_path=$1
+max_log2n=$2
 
-for ((i=0; i<=26; i+=1))
+for ((i=0; i<=$max_log2n; i+=1))
 do
 	n=$((2 ** i))
     $(dirname "$0")/../rust/target/release/generate_permutations $n > "$dir_path/2^$i.perm"
