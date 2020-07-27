74826326c2a0e2808a25b01125417210725cd8c7
RyanGibb <ryangibb321@gmail.com>
2020-07-27 12:49:57 +0100

    Changed benchmarking to show time not CPU time.

--- benchmarking/run.sh
+++ benchmarking/run.sh
@@ -5,7 +5,7 @@ TARGET_TIME_NS=10000000 # 10 milliseconds
 PERMUTATION_FILES=$(ls -v /cs/scratch/rtg2/permutations/*.perm)
 
 if [ ! -f $out_file ]; then
-    echo "Timestamp, Repetition, n, Executable, Iterations, CPU Time (ns), Host" > $out_file
+    echo "Timestamp, Repetition, n, Executable, Iterations, Time (ns), Host" > $out_file
 fi
 
 get_cpu_time () {
@@ -18,15 +18,15 @@ cargo build --release --manifest-path $(dirname "$0")/../rust/Cargo.toml || exit
 for ((rep=1; rep<=REPETITIONS; rep++)); do
 	for file in $PERMUTATION_FILES; do
 		for executable in "$@"; do
-			cpu_time_ns=0
+			time_ns=0
 			new_iterations=1
-			while [ $cpu_time_ns -le $TARGET_TIME_NS ]; do
+			while [ $time_ns -le $TARGET_TIME_NS ]; do
 				iterations=$new_iterations
 				timestamp=$(date)
-				cpu_time_ns=$(get_cpu_time)
-				if [ "$cpu_time_ns" = "" ]; then
+				time_ns=$(get_cpu_time)
+				if [ "$time_ns" = "" ]; then
 					echo "An error occured!"
-					cpu_time_ns="Error"
+					time_ns="Error"
 					break
 				fi
 				new_iterations=$(($iterations * 2))
