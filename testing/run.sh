d5fc9747250f0a38930aa304b72190345e8643f7
RyanGibb <ryangibb321@gmail.com>
2020-07-29 13:12:07 +0100

    Fixed testing run.sh

--- testing/run.sh
+++ testing/run.sh
@@ -5,9 +5,10 @@ cargo build --release --manifest-path $(dirname "$0")/../rust/Cargo.toml || exit
 for executable in "$@"; do
 	echo $executable
 	out_dir=$(dirname "$0")/$(basename "$executable" .sh)
-	mkdir -p $out_dir
+	mkdir -p "$out_dir"
 	for file in $(dirname "$0")/../permutations/*; do
 		echo "$file"
-		cat "$file" | ./"$executable" | tail -n 1 > $out_dir/$(basename "$file" .perm).out
+		file_base=$(basename "$file" .perm)
+		cat "$file" | ./"$executable" | tail -n 1 > "$out_dir/$file_base.out"
 	done
 done
