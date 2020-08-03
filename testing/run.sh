166109e898026ebdb4849acc8a3d2afaef59faf4
RyanGibb <ryangibb321@gmail.com>
2020-08-03 16:11:22 +0100

    Fixed testing

--- testing/run.sh
+++ testing/run.sh
@@ -9,6 +9,12 @@ for executable in "$@"; do
 	for file in $(dirname "$0")/../permutations/*; do
 		echo "$file"
 		file_base=$(basename "$file" .perm)
-		cat "$file" | ./"$executable" | tail -n 1 > "$out_dir/$file_base.out"
+		cat "$file" | ./"$executable" | tail -n +2 > "$out_dir/$file_base.out"
+		# exit if executable fails
+		return_code=${PIPESTATUS[1]}
+		if [ $return_code -ne 0 ]; then
+			echo "Executable failed!"
+			exit $return_code
+		fi
 	done
 done
